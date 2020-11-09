18795cee4f643f2cc8cce590efef6abb4e69ee69
Andrew <andrewhutchison000@gmail.com>
2020-11-09 12:47:21 +1000

    Stub out Annotation class, define AnnotationKind

--- sourcetools/annotate.py
+++ sourcetools/annotate.py
@@ -1,5 +1,32 @@
+from enum import Enum, auto
+
 from .source import Range
 
+class AnnotationKind(Enum):
+    NOTE = auto()
+    WARN = auto()
+    ERROR = auto()
+    FATAL = auto()
+
 class Annotation:
-    def __init__(self, range_: Range):
-        pass
+    def __init__(self, range_: Range, msg: str, kind: AnnotationKind):
+        self._range = range_
+        self._msg = msg
+        self._kind = kind
+        self._full_lines = range_.full_lines()
+
+    @property
+    def range(self):
+        return self._range
+
+    @property
+    def message(self):
+        return self._msg
+
+    @property
+    def kind(self):
+        return self._kind
+
+    @property
+    def full_lines(self):
+        return self._full_lines
